06abcb6910431f5678beb69163eac4a8fd03e2e2
leafstonels <hadiayyoobmk99@gmail.com>
2026-01-06 22:14:21 +0530

    Add results for on-stage program 'Classical Dance'
    Added results for the first on-stage program.

--- js/data.js
+++ js/data.js
@@ -51,4 +51,22 @@ const offStagePrograms = [
   // continue till 28
 ];
 
+onStagePrograms[on1].first = {
+  student: "Anjali S",
+  team: "Team Alpha",
+  points: 100
+};
+
+onStagePrograms[on1].second = {
+  student: "Rahul M",
+  team: "Team Beta",
+  points: 70
+};
+
+onStagePrograms[on1].third = {
+  student: "Sneha P",
+  team: "Team Gamma",
+  points: 50
+};
+
 

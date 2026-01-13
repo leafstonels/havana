82c3f9f5ddc06d6b4688c3a5fd5fcec29255c40c
leafstonels <hadiayyoobmk99@gmail.com>
2026-01-14 01:51:57 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ const TOP_SCORERS = {
     class: "bca",
     group: "konayadi",
     score: "69",
-    photo: "/assets/deva.png"
+    photo: "photo: "https://picsum.photos/400"
   },
   2: {
     name: "shiji",
@@ -452,6 +452,7 @@ setInterval(async () => {
     renderLeaderboard();
   }
 }, 15000); // every 15 seconds
+
 
 
 
